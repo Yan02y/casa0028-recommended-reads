c233f36ce74a136f33aebd609c74e7afeb53fcd0
ddunc23 <d.hay@ucl.ac.uk>
2025-10-16 09:46:25 +0100

    first commit

---
+++ src/components/MainMenu.jsx
@@ -0,0 +1,11 @@
+export default function MainMenu() {
+    return (
+        <header className="bg-white">
+            <div className="mx-auto h-16 pt-4 gap-8 px-4 sm:px-6 lg:px-8">
+                <h3 className="text-center text-lg font-bold italic">
+                    📚 OpenPlaques Reading Recommendations
+                </h3>
+            </div>
+        </header>
+    )
+}
